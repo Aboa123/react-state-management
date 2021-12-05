6308b38d1d606ce10f5f918363845f5736a92953
Aboa123 <wlxm9999@gmail.com>
2021-12-05 21:36:35 +0900

    delete comment

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -4,30 +4,6 @@ import {
   useForm
 } from 'react-hook-form';
 
-// const ToDoList = () => {
-//   const [toDo, setToDo] = useState("");
-//   const onchange = (e: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = e;
-//     setToDo(value);
-//   }
-
-//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     console.log(toDo)
-//   }
-
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onchange} value={toDo} placeholder={"write to do"} />
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
-
 interface IFormData {
   email: string;
   nickName: string;
